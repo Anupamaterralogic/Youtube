110320758175ee97fbe6976ba1e58f6d903ae374
Anupama <anupama.g@terralogic>
2022-05-30 11:10:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
+export interface Category {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube';
+  Categories: Category[] = [
+    { name: 'All' },
+    { name: 'Live' },
+    { name: 'Gaming' },
+    { name: 'Music' },
+    { name: 'Bhavya Gandi' },
+    { name: 'Tamil Cinema' },
+    { name: 'T-series' },
+    { name: 'Yash' },
+    { name: 'Trailers' },
+    { name: 'Cricket' },
+    { name: 'Gadgets' },
+    { name: 'Trains' },
+    { name: 'Love Songs' },
+    { name: 'Eating' },
+    { name: 'Tourist Places' },
+    { name: 'Conversation' },
+    { name: 'Cars' },
+    { name: 'Electronic Music' },
+    { name: 'Recently Uploaded' },
+  ];
+  // hidden = false;
+
+  // toggleBadgeVisibility() {
+  //   this.hidden = !this.hidden;
+  // }
+  showFiller = true;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatCardModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
